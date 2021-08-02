687a743332d1ffa784ca653920088e1f11bc1aed
Lingling <misspighead@gmail.com>
2021-08-02 22:02:25 +0800

    引用asset路徑，直接設成絕對路徑避免錯誤

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -4,10 +4,10 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/x-icon" href="image/favicon.ico" />
+  <link rel="icon" type="image/x-icon" href="/image/favicon.ico" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
-  <link href="css/customBackend.css" rel="stylesheet" />
+  <link href="/css/customBackend.css" rel="stylesheet" />
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <title>後台管理 - @yield('title')</title>
 </head>
