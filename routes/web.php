a22f005d01f29ad398889d6222c052a7f83605b2
Lingling <misspighead@gmail.com>
2021-08-02 22:38:03 +0800

    在Route使用prefix('admin'), 並建立AdminController群組化後台頁面

--- routes/web.php
+++ routes/web.php
@@ -11,32 +11,15 @@
 |
 */
 
+use App\Http\Controllers\AdminController;
+
 Route::get('/', function () {
     return view('frontend.main');
 });
 
 Route::prefix('admin')->group(function () {
-    Route::get('/', function () {
-        return view('backend.main');
-    });
-    Route::get('/banner', function () {
-        return view('backend.banner');
-    });
-    Route::get('/orders', function () {
-        return view('backend.orders');
-    });
-    Route::get('/products', function () {
-        return view('backend.products');
-    });
-    Route::get('/news', function () {
-        return view('backend.news');
-    });
-    Route::get('/members', function () {
-        return view('backend.members');
-    });
-    Route::get('/category', function () {
-        return view('backend.category');
-    });
+    Route::get('/', 'AdminController@index');
+    Route::get('/{content}','AdminController@content');
 });
 
 Auth::routes();
