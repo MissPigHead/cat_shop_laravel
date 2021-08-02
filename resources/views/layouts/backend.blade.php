e95538534a46c9939efec7639e25c2af32e18756
Lingling <misspighead@gmail.com>
2021-08-02 22:54:13 +0800

    shortcut icon 路徑寫錯了

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/x-icon" href="/image/favicon.ico" />
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
   <link href="/css/customBackend.css" rel="stylesheet" />
