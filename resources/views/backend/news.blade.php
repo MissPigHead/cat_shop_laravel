01d385b3c2616546eaa510851f40142c8a191e71
Lingling <misspighead@gmail.com>
2021-08-06 09:24:47 +0800

    前端view 用ajax和api 互動

--- resources/views/backend/news.blade.php
+++ resources/views/backend/news.blade.php
@@ -1,143 +1,246 @@
 @extends('layouts.backend')
 @section('title','育貓新知區')
 @section('content')
-      <div class="col-9 content">
-        <!-- 新增區 -->
-        <div class="row justify-content-center">
-          <button type="button" class="btn btn-info" data-toggle="modal" data-target="#addMember">新增促銷資訊</button>
-        </div>
-        <div class="modal fade" id="addMember" tabindex="-1" aria-labelledby="addMemberLabel" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="addMemberLabel">新增促銷資訊</h5>
+  <div class="col-9 content">
+    <!-- 新增區 -->
+    <div class="row justify-content-center">
+      <button type="button" class="btn btn-info" data-toggle="modal" data-target="#storeNews">新增文章</button>
+    </div>
+    <!-- 新增用Modal -->
+    <div class="modal fade" id="storeNews" tabindex="-1" aria-labelledby="storeNewsLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="storeNewsLabel">新增文章</h5>
+          </div>
+          <div class="modal-body">
+              <div class="form-group row">
+                <label for="title" class="col-2 col-form-label">標題</label>
+                <div class="col-9">
+                  <input type="text" class="form-control" id="title">
+                </div>
               </div>
-              <div class="modal-body">
-                <form>
-                  <div class="form-group row">
-                    <label for="title" class="col-2 col-form-label">標題</label>
-                    <div class="col-9">
-                      <input type="text" class="form-control" id="title">
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <label for="article" class="col-2 col-form-label">內容</label>
-                    <div class="col-9">
-                      <textarea class="form-control" id="article" rows="5"></textarea>
-                    </div>
-                  </div>
-                </form>
+              <div class="form-group row">
+                <label for="article" class="col-2 col-form-label">內容</label>
+                <div class="col-9">
+                  <textarea class="form-control" id="article" rows="5"></textarea>
+                </div>
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-info" id="submitNews">確認</button>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
-              </div>
-            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-info" onclick="postNews()">確認</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
           </div>
         </div>
+      </div>
+    </div>
+    <!-- 修改用Modal -->
+    <div class="modal fade" id="updateNews" tabindex="-1" aria-labelledby="updateNewsLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="updateNewsLabel">編輯文章</h5>
+          </div>
+          <div class="modal-body">
+              <div class="form-group row">
+                <label for="title" class="col-2 col-form-label">標題</label>
+                <div class="col-9">
+                  <input type="text" class="form-control" id="updateTitle">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label for="article" class="col-2 col-form-label">內容</label>
+                <div class="col-9">
+                  <textarea class="form-control" id="updateArticle" rows="5"></textarea>
+                </div>
+              </div>
+          </div>
+          <div class="modal-footer">
+            <input type="hidden" value="">
+            <button type="button" class="btn btn-info" onclick="updateNews()">確認</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script> 
+      function postNews(){
+        data={
+          title: $('#title').val(),
+          article: $('#article').val(),
+          _token: '{{csrf_token()}}' 
+          // Laravel強制要求要防範CSRF( Cross-site request forgery)攻擊 往資料庫送資料時要記得加！
+        }
+        $.ajax({
+          url: "/api/news",
+          method: "POST",
+          dataType: "text", // "json"：ajax呼叫成功，但返回資料非json，所以進入error，且狀態碼200
+          data: data,
+          success: function (result) {
+              console.log("非同步呼叫返回成功,result:"+result);
+              alert('新增成功')
+              location.reload()
+          },
+          error: function (result,XMLHttpResponse, textStatus, errorThrown) {
+              console.log(result)
+              console.log("1 非同步呼叫返回失敗,XMLHttpResponse.readyState:"+XMLHttpResponse.readyState);
+              console.log("2 非同步呼叫返回失敗,XMLHttpResponse.status:"+XMLHttpResponse.status);
+              console.log("3 非同步呼叫返回失敗,textStatus:"+textStatus);
+              console.log("4 非同步呼叫返回失敗,errorThrown:"+errorThrown);
+              alert('新增失敗，請截圖提供給管理員，謝謝！\r\nXMLHttpResponse.readyState:'+XMLHttpResponse.readyState+'\r\nXMLHttpResponse.status:'+XMLHttpResponse.status+'\r\ntextStatus:'+textStatus+'\r\nerrorThrown:'+errorThrown)
+              location.reload()
+          }
+        })
+      }
+    </script>
 
 
-        <script>
-          $('#submitNews').click(function(){
-            data={
-              title: $('#title').val(),
-              article: $('#article').val(),
-              _token: '{{csrf_token()}}' 
-              // Laravel強制要求要防範CSRF( Cross-site request forgery)攻擊 往資料庫送資料時要記得加！
-            }
-            $.ajax({
-              url: "/api/news",
-              method: "POST",
-              dataType: "json",
-              data: data,
-              success: function(res){console.log("PASS",res)},
-              error: function(err){console.log(err)},
-            })
-          })
-        </script>
+    <!-- 顯示區 -->
+    <table class="mt-4 table  table-bordered table-hover">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col" class="col-1">更新日期</th>
+          <th scope="col" class="col-2">標題</th>
+          <th scope="col" class="col-5">內容</th>
+          <th scope="col" class="col-2">圖片</th>
+          <th scope="col" class="col-1">狀態</th>
+          <th scope="col" class="col-1">操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($news as $news)
+          <tr>
+            <td>{{substr($news->updated_at,0,10)}}</td>
+            <td class="text-wrap text-left">{{$news->title}}</td>
+            <td class="text-wrap text-left">{{mb_substr($news->article,0,50)}}...</td>
+            <td>
+              <img src="{{$news->image_path}}" class="w-100">
+            </td>
 
+            @if ($news->show==1)
+              <td>已發佈</td>
+              <td>
+                <button type="button" class="btn btn-outline-secondary w-100" title="停止顯示此文章" onclick="hide({{$news->id}})">
+            @else
+              <td>隱藏中</td>
+              <td>
+                <button type="button" class="btn btn-outline-secondary w-100" title="公開發佈此文章" onclick="show({{$news->id}})">
+            @endif
 
-        <!-- 顯示區 -->
-        <table class="mt-4 table  table-bordered table-hover">
-          <thead class="thead-dark">
-            <tr>
-              <th scope="col" class="col-1">更新日期</th>
-              <th scope="col" class="col-3">標題</th>
-              <th scope="col" class="col-5">內容</th>
-              <th scope="col" class="col-1">狀態</th>
-              <th scope="col" class="col-2">操作</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>2021/07/22</td>
-              <td class="text-wrap text-left">我是名稱很長的名字</td>
-              <td class="text-wrap text-left">某種鬱悶什麼時候專門祝福司機我說加大，斑竹保存，失去規劃，小學思路怎麼高度我說頻率才會這是你知道站點，因而理想接到相冊提示自從細節製造走了其實關西來的職務給我，面對很多無派回應就有一批標準股權稱為流程反映確保地面機構，風雲提升臉色推坑大魔王，負責人，投訴。</td>
-              <td>顯示中</td>
-              <td>
-                <button type="button" class="btn btn-outline-secondary">
-                  <i class="far fa-eye-slash"></i>
-                </button>
-                <button type="button" class="btn btn-outline-secondary">
-                  <i class="fas fa-edit"></i>
-                </button>
-                <button type="button" class="btn btn-outline-secondary">
-                  <i class="fas fa-trash-alt"></i>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td>2021/07/22</td>
-              <td class="text-wrap text-left">我是名稱很長很長很長很長很長很長很長很長的名字</td>
-              <td class="text-wrap text-left">某種鬱悶什麼時候專門祝福司機我說加大，斑竹保存，失去規劃，小學思路怎麼高度我說頻率才會這是你知道站點，因而理想接到相冊提示自從細節製造走了其實關西來的職務給我，面對很多無派回應就有一批標準股權稱為流程反映確保地面機構，風雲提升臉色推坑大魔王，負責人，投訴。</td>
-              <td>顯示中</td>
-              <td>
-                <button type="button" class="btn btn-outline-secondary">
-                  <i class="far fa-eye-slash"></i>
-                </button>
-                <button type="button" class="btn btn-outline-secondary">
-                  <i class="fas fa-edit"></i>
-                </button>
-                <button type="button" class="btn btn-outline-secondary">
-                  <i class="fas fa-trash-alt"></i>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td>2021/07/22</td>
-              <td class="text-wrap text-left">我是名稱很長很長很長很長很長的名字</td>
-              <td class="text-wrap text-left">某種鬱悶什麼時候專門祝福司機我說加大，斑竹保存，失去規劃，小學思路怎麼高度我說頻率才會這是你知道站點，因而理想接到相冊提示自從細節製造走了其實關西來的職務給我，面對很多無派回應就有一批標準股權稱為流程反映確保地面機構，風雲提升臉色推坑大魔王，負責人，投訴。</td>
-              <td>已隱藏</td>
-              <td>
-                <button type="button" class="btn btn-outline-secondary">
-                  <i class="far fa-eye"></i>
-                </button>
-                <button type="button" class="btn btn-outline-secondary">
-                  <i class="fas fa-edit"></i>
-                </button>
-                <button type="button" class="btn btn-outline-secondary">
-                  <i class="fas fa-trash-alt"></i>
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td>2021/07/22</td>
-              <td class="text-wrap text-left">我是名稱很長很長的名字</td>
-              <td class="text-wrap text-left">某種鬱悶什麼時候專門祝福司機我說加大，斑竹保存，失去規劃，小學思路怎麼高度我說頻率才會這是你知道站點，因而理想接到相冊提示自從細節製造走了其實關西來的職務給我，面對很多無派回應就有一批標準股權稱為流程反映確保地面機構，風雲提升臉色推坑大魔王，負責人，投訴。</td>
-              <td>顯示中</td>
-              <td>
-                <button type="button" class="btn btn-outline-secondary">
-                  <i class="far fa-eye-slash"></i>
-                </button>
-                <button type="button" class="btn btn-outline-secondary">
-                  <i class="fas fa-edit"></i>
-                </button>
-                <button type="button" class="btn btn-outline-secondary">
-                  <i class="fas fa-trash-alt"></i>
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>    
+                <i class="far {{($news->show==1)?'fa-eye-slash':'fa-eye'}}"></i>
+              </button><br>
+              <button type="button" class="btn btn-outline-secondary w-100 my-1" title="修改編輯" data-toggle="modal" data-target="#updateNews" onclick="showNews({{$news->id}})">
+                <i class="fas fa-edit"></i>
+              </button><br>
+              <button type="button" class="btn btn-outline-secondary w-100" title="刪除資料" onclick="destroy({{$news->id}})">
+                <i class="fas fa-trash-alt"></i>
+              </button>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+    <script>
+      function showNews(id){
+        $.ajax({
+          url: "/api/news/"+id,
+          method: "GET",
+          dataType: "json", // 注意抓回資料型態
+          success: function (result) {
+              $('#updateTitle').val(result.title)
+              $('#updateArticle').val(result.article)
+              $('#updateNews input[type=hidden]').val(result.id)
+              // $('#updateNews button').on('click',updateNews(result.id))
+          }
+        })
+      }
+
+      function updateNews(){
+        id=$('#updateNews input[type=hidden]').val()
+        data={
+          title: $('#updateTitle').val(),
+          article: $('#updateArticle').val(),
+          _token: '{{csrf_token()}}' 
+          // Laravel強制要求要防範CSRF( Cross-site request forgery)攻擊 往資料庫送資料時要記得加！
+        }
+        $.ajax({
+          url: "/api/news/"+id,
+          method: "PATCH",
+          dataType: "text",
+          data: data,
+          success: function (result) {
+              alert('修改成功')
+              location.reload()
+          },
+          error: function (result,XMLHttpResponse, textStatus, errorThrown) {
+              // console.log(result)
+              alert('修改失敗，請通知管理員！')
+              location.reload()
+          }
+        })
+      }
+
+      function show(id){
+        $.ajax({
+          url: "/api/news/"+id,
+          method: "PATCH",
+          dataType: "text",
+          data: {
+            show: 1,
+            _token: '{{csrf_token()}}',
+          },
+          success: function (result) {
+            alert('修改成功')
+            location.reload()
+          },
+          error: function (result,XMLHttpResponse, textStatus, errorThrown) {
+            alert('修改失敗，請通知管理員！')
+            location.reload()
+          }
+        })
+      }
+
+      function hide(id){
+        $.ajax({
+          url: "/api/news/"+id,
+          method: "PATCH",
+          dataType: "text",
+          data: {
+            show: 0,
+            _token: '{{csrf_token()}}',
+          },
+          success: function (result) {
+            alert('修改成功')
+            location.reload()
+          },
+          error: function (result,XMLHttpResponse, textStatus, errorThrown) {
+            alert('修改失敗，請通知管理員！')
+            location.reload()
+          }
+        })
+      }
+
+      function destroy(id){
+        var destroy=confirm("確認刪除？") 
+        if (destroy){ 
+          $.ajax({
+          url: "/api/news/"+id,
+          method: "DELETE",
+          dataType: "text",
+          data: {
+            _token: '{{csrf_token()}}',
+          },
+          success: function (result) {
+            alert('刪除成功')
+            location.reload()
+          },
+          error: function (result,XMLHttpResponse, textStatus, errorThrown) {
+            alert('刪除失敗，請通知管理員！')
+            location.reload()
+          }
+        })
+        } 
+      }
+    </script>
+  </div>    
 @endsection
--- resources/views/backend/sidebar.blade.php
+++ resources/views/backend/sidebar.blade.php
@@ -4,6 +4,6 @@
   <a href="/admin/products"><div>商品區</div></a>
   <a href="/admin/members"><div>會員區</div></a>
   <a href="/admin/orders"><div>訂單區</div></a>
-  <a href="/admin/news"><div>育毛新知區</div></a>
+  <a href="/admin/news"><div>育貓新知區</div></a>
   <a href="/admin/banner"><div>Banner 廣告區</div></a>
 </div>
