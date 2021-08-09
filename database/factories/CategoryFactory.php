67c7ab8314cbab84baf13cfe25edb191730620d8
Lingling <misspighead@gmail.com>
2021-08-09 14:22:20 +0800

    修改測試資料種子 較容易辨識

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -9,7 +9,7 @@ $factory->define(Category::class, function (Faker $faker) {
     return [
         'title'=>$faker->text(10),
         'parent'=>rand(1,4),
-        'show'=>$faker->boolean($chanceOfGettingTrue = 90),
+        'show'=>$faker->boolean($chanceOfGettingTrue = 80),
         'created_at' => "2020-06-20 19:12:54",
         'updated_at' => $faker->dateTimeThisYear('2021-06-30','Asia/Taipei'),
     ];
--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use App\Models\Category;
+use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
 {
@@ -13,12 +13,15 @@ class CategorySeeder extends Seeder
     public function run()
     {
         Category::truncate(); // 清空所有表格資料
-        for ($i=0; $i <30 ; $i++) { 
-            $category=factory(Category::class)->make();
-            if($i<4 || $i==15 || $i==25){
+        for ($i = 0; $i < 30; $i++) {
+            $category = factory(Category::class)->make();
+            if ($i < 4 || $i == 10 || $i == 20) {
                 $category->parent = 0;
+                $category->title = "Parent" . $i;
+            } else {
+                $category->title = "Child" . $i;
             }
-            $category->order = Category::where('parent',$category->parent)->count()+1;
+            $category->order = Category::where('parent', $category->parent)->count() + 1;
             $category->save();
         }
     }
