3ee19803b4cc3390b473b9070765f112f07230bb
Lingling <misspighead@gmail.com>
2021-08-18 11:52:00 +0800

    後台news 增加功能：更新圖片時可刪除圖片 或取代舊圖(目前將舊圖移到另資料夾，不直接刪除)

--- app/Http/Controllers/API/NewsController.php
+++ app/Http/Controllers/API/NewsController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\News;
 use Facade\FlareClient\Http\Response;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 
 class NewsController extends Controller
@@ -15,7 +17,7 @@ class NewsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index() // 取全部
     {
         $news = News::orderBy('updated_at', 'desc')->get();
         $data = ['news' => $news];
@@ -28,30 +30,28 @@ class NewsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request) // 儲存資料
     {
-        // $news=News::make($request->all());
+        $news = News::make($request->all());
 
-        // if($request->hasFile('image')){
-        //     $dir_sub = "news";
-        //     $storage_path = "public/" . $dir_sub;
-        //     $request->validate([
-        //         'image' => 'required|mimes:jpg,jpeg,bmp,png|max:2048',
-        //     ]); // 驗證失敗的部分，還沒寫
-        //     $file_name = time() . $request->image->getClientOriginalName();
-        //     $request->image->storeAs($storage_path, $file_name);
-        //     // 以上是將image 存到public 指定路徑
+        if ($request->hasFile('image')) {
+            $dir_sub = "news";
+            $storage_path = "public/" . $dir_sub;
+            // $request->validate([
+            //     'image' => 'required|mimes:jpg,jpeg,bmp,png|max:2048',
+            // ]); // 驗證失敗的部分，還沒寫
+            $file_name = time() . $request->image->getClientOriginalName();
+            $request->image->storeAs($storage_path, $file_name);
+            // 以上是將image 存到public 指定路徑
 
-        //     // 以下是寫入資料庫內容
-        //     $public_path = "/storage/" . $dir_sub . "/" . $file_name;
+            // 以下是寫入資料庫內容
+            $public_path = "/storage/" . $dir_sub . "/" . $file_name;
 
-        //     $news->image_path=$public_path;
-        // }
+            $news->image_path = $public_path;
+        }
 
-        // $news->save();
-        // return redirect('/admin/news');
-
-        return back()->withError('There was an error');
+        $news->save();
+        return back();
     }
 
     /**
@@ -60,7 +60,7 @@ class NewsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($id) // 取得id資料
     {
         $news = News::findOrFail($id);
         return $news;
@@ -73,8 +73,7 @@ class NewsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-
-    public function update(Request $request, $id)
+    public function update(Request $request, $id) // 更新id資料
     {
         $news = News::find($id);
         $news->update($request->all());
@@ -86,43 +85,79 @@ class NewsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($id) // 刪除id資料
     {
         News::destroy($id);
     }
 
-    public function updateWithFile(Request $request)
+    public function updateWithFile(Request $request) //先存圖片 再更新資料
     {
-        $dir_sub = "news";
+        /* 驗證 */
+        $rules = [
+            'title' => 'required|max:30',
+            'article' => 'required|max:1000',
+            'image' => 'mimes:jpg,jpeg,bmp,png|max:2048',
+        ];
+        $message = [
+            'title.required' => '標題不得為空白',
+            'title.max' => '最多30個字',
+            'article.required' => '內容不得為空白',
+            'article.max' => '最多1000個字',
+            'image.mimes' => '請使用 jpg, jpeg, bmp, png 圖片',
+            'image.max' => '圖片尺寸需在2MB以下'
+        ];
+        $re = $request->validate($rules, $message);
 
-        $storage_path = "public/" . $dir_sub;
-
-        // $request->validate([
-        //     'image' => 'required|mimes:jpg,jpeg,bmp,png|max:2048',
-        // ]);
-        // 驗證的部分，還沒寫
-        // 更新圖片 是否刪除舊圖片 如何刪除或辨識
-
-        // $file_name = time() . $request->image->getClientOriginalName(); // 原始檔名
-        $file_name = time() . $request->image->hashName(); // 雜湊名稱
-
-        $request->image->storeAs($storage_path, $file_name);
-        // 以上是將image 存到public 指定路徑
-
-        $public_path = "/storage/" . $dir_sub . "/" . $file_name;
-        // 以上是寫入資料庫內容
-
-        $request->image_path = $public_path;
-
+        /* 撈出要更新的資料 */
         $re = $request->input();
-        $re['image_path'] = $public_path;
-
         $news = News::find($re['id']);
+        /* 處理圖片*/
+        if ($request->image) { // 有上傳圖片
+            if ($news->image_path) { // 修改：有回傳圖片檔案 / 有舊圖片路徑; 移除舊圖片
+                if (File::exists($news->image_path)) {
+                    Storage::move("public" . substr($news->image_path, 8), "user_delete" . substr($news->image_path, 8)); // 暫時移到這個路徑，再定期刪除（！？）
+                    File::delete($path); // 刪除
+
+                    File::replace($path, $content); // 写入文件，存在的话覆盖写入
+
+                }
+            } // else 新增：有回傳圖片檔案 / 無舊圖片路徑; 單純新增圖片
+            $public_path = $this->saveFile($request->image); // 存圖片
+        } else { // 無上傳圖片
+            if ($news->image_path) { // 無回傳圖片檔案 / 有舊圖片路徑 >> 有可能沿用舊圖 or 刪除圖片
+                if ($request->original_image) { // 沿用舊圖不變化
+                    $public_path = $news->image_path;
+                } else { // 刪除舊圖，不再使用圖片
+                    if (File::exists($news->image_path)) {
+                        Storage::move("public" . substr($news->image_path, 8), "user_delete" . substr($news->image_path, 8)); // 暫時移到這個路徑，再定期刪除（！？）
+                    }
+                    $public_path = '';
+                }
+            } else { // 一直都不使用圖片：無回傳圖片檔案 / 無舊圖片路徑
+                $public_path = '';
+            }
+        }
+
+        /* 整理要做更新的項目 */
+        $re['image_path'] = $public_path;
         unset($re['_token']);
         unset($re['id']);
 
         $news->update($re);
 
-        return redirect('/admin/news');
+        return back();
     }
+
+    public function saveFile($file) // 儲存圖片，回傳圖片public路徑
+    {
+        $dir_sub = "news";
+        $storage_path = "public/" . $dir_sub;
+        // $file_name = time() . $request->image->hashName(); // 雜湊名稱
+        $file_name = time() . $file->hashName(); // 雜湊名稱
+        // $file_name = time() . $request->image->getClientOriginalName(); // 原始檔名
+        $file->storeAs($storage_path, $file_name); // 將image 存到指定路徑
+        $public_path = "/storage/" . $dir_sub . "/" . $file_name; // 圖片路徑
+        return $public_path;
+    }
+
 }
--- resources/views/backend/news.blade.php
+++ resources/views/backend/news.blade.php
@@ -54,7 +54,7 @@
               <label for="image" class="col-2 col-form-label">圖片</label>
               <div class="col-9">
                 <input type="file" name="image" id="imageNew">
-                <p class="small text-dark">*可留空或選擇1張圖片</p>
+                <p class="small text-dark my-2">*可留空或選擇1張2MB以下jpg, jpeg, bmp, png 圖檔</p>
               </div>
             </div>
             <img class="w-100" id="previewNew">
@@ -92,14 +92,20 @@
             </div>
             <div class="form-group row">
               <label for="article" class="col-2 col-form-label">圖片</label>
-              <div class="col-9">
-                <button type="button" class="btn btn-info" id="chooseImage" onclick="addInput()">修改圖片</button>
-                <input type="file" class="my-2 d-none w-100" name="image" id="imageUpdate">
-                <button type="button" class="btn btn-light text-secondary d-none" id="originalImage"
-                  onclick="">使用原存檔圖片</button>
+              <div class="col-9 row">
+                <button type="button" class="btn btn-info ml-3" id="chooseImage" onclick="addInput()">修改圖片</button>
+                <input type="file" class="my-2 col-12" name="image" id="imageUpdate">
+
+
+                <div class="btn-group-toggle ml-3" data-toggle="buttons">
+                    <label class="btn btn-light text-secondary">
+                      <input type="radio" name="original_image" id="originalImage" checked><span> 使用原存檔圖片</span>
+                    </label>
+                  </div>
+
               </div>
             </div>
-            <img src="" class="w-100 my-2" id="previewUpdate"><span></span>
+            <img src="" class="w-100 my-2" id="previewUpdate"><p id="noImageRecord"></p>
           </div>
           <div class="modal-footer">
             <input type="hidden" name="id" value>
@@ -160,8 +166,10 @@
     </tbody>
   </table>
   <script>
+    $('#imageUpdate').hide()
+
     $('[type=reset]').click(function(e) { // 新增用Modal: reset 時移除預覽圖片
-      $('#imageNew + p').removeClass('d-none') // 顯示上傳圖片的提醒字
+      $('#imageNew + p').show() // 顯示上傳圖片的提醒字
       $('#previewNew').attr('src', '') // 清乾淨之前preview 的資料
     })
 
@@ -170,41 +178,43 @@
         const file = this.files[0];
         const fr = new FileReader();
         fr.onload = function(e) {
-          $('#imageNew + p').addClass('d-none')
+          $('#imageNew + p').hide()
           $('#previewNew').attr('src', e.target.result);
         };
         fr.readAsDataURL(file);
       } else { // 沒圖就出現提示
         $('#previewNew').attr('src', '');
-        $('#imageNew + p').removeClass('d-none')
+        $('#imageNew + p').show()
       }
     });
 
     function addInput() { // 修改用Modal: 按下修改圖片
-      $("#chooseImage").addClass('d-none') // 修改圖片
-      $('#imageUpdate').removeClass('d-none') // 顯示 input file
-      $('#originalImage').removeClass('d-none') // 使用原圖
-      $('#deleteImage').removeClass('d-none') // 刪除圖片
+      $("#chooseImage").hide() // 修改圖片
+      $('#imageUpdate').show() // 顯示 input file
+      $('#originalImage').parent().parent().show() // 使用原圖
+      $('#deleteImage').show() // 刪除圖片
     }
 
     function deleteImg(id) { // 修改用Modal: 按下刪除圖片
-      $("#deleteImage").addClass('d-none') // 刪除圖片
-      $('#originalImage').removeClass('d-none') // 使用原圖
-      $('#chooseImage').removeClass('d-none') // 新增圖片
+      $("#deleteImage").hide() // 刪除圖片
+      $('#originalImage').parent().parent().show() // 使用原圖
+      $("#originalImage").prop('checked',false)
+      $('#chooseImage').show() // 新增圖片
       $('#previewUpdate').attr('src', '') // 預覽 --> 移除
-      $('#imageUpdate').addClass('d-none') // input file
+      $('#imageUpdate').hide() // input file
       $('#imageUpdate').val('') // input file
     }
 
     function originalImg(src) { // 修改用Modal: 使用原圖片
       if (src) {
-        $("#deleteImage").removeClass('d-none') // 刪除圖片 --> 顯示
+        $("#deleteImage").show() // 刪除圖片 --> 顯示
       }
-      $('#originalImage').addClass('d-none') // 使用原圖 --> 移除
-      $("#chooseImage").removeClass('d-none') // 新增圖片 --> 顯示
+      $('#originalImage').parent().parent().hide()// 使用原圖 --> 移除
+      $("#originalImage").prop('checked',true)
+      $("#chooseImage").show() // 新增圖片 --> 顯示
       $('#imageUpdate').val('') // 移除 input file
-      $('#imageUpdate').addClass('d-none') // 移除 input file
-
+      $('#imageUpdate').hide() // 移除 input file
+      $("#noImageRecord").show()
       $('#previewUpdate').attr('src', src) // 預覽 --> 原圖
     }
 
@@ -216,24 +226,24 @@
           $('#previewUpdate').attr('src', e.target.result);
         };
         fr.readAsDataURL(file);
-        $("#previewUpdate~span").addClass('d-none') // 移除沒有圖片的提示
+        $("#noImageRecord").hide() // 移除沒有圖片的提示
+        $("#originalImage").prop('checked',false)
       } else {
         $('#previewUpdate').attr('src', '');
-        $("#previewUpdate~span").removeClass('d-none')
+        $("#noImageRecord").show()
+        $("#originalImage").prop('checked',true) // 未上傳圖片時，預設使用原圖，只有按下delete才刪除
       }
     })
 
     function getNews(id) { // 修改用Modal: 將要進行編輯的news 資訊塞入Modal
       // 將Modal 內的元件回到預設狀態
-      $("#chooseImage").removeClass('d-none')
-      $('#imageUpdate').addClass('d-none')
-      $('#originalImage').addClass('d-none')
+      $("#chooseImage").show()
+      $('#imageUpdate').hide()
+      $('#originalImage').parent().parent().hide()
       $("#deleteImage").remove()
-      $("#previewUpdate~span").text('')
       $('#previewUpdate').attr('src', '')
 
-      // ajax 取回要編輯的物件資訊
-      $.ajax({
+      $.ajax({ // ajax 取回要編輯的物件資訊
         url: "/api/news/" + id,
         method: "GET",
         dataType: "json", // 注意抓回資料型態
@@ -243,18 +253,20 @@
           $('#updateForm input[type=hidden]').val(result.id) // 修改Modal這裡藏id
 
           if (result.image_path) { // 有image_path資料: 預覽 + 修改 + 刪除圖片
+            $("#noImageRecord").text('')
             $('#previewUpdate').attr('src', result.image_path)
             $("#chooseImage").text('修改圖片')
-            $('#originalImage').text('使用原存檔圖片')
+            $('#originalImage~span').text('使用原存檔圖片')
             $('#imageUpdate').after(`
-                <button type="button" class="btn btn-secondary" id="deleteImage">刪除圖片</button>`)
+            <button type="button" class="btn btn-secondary ml-3" id="deleteImage">刪除圖片</button>`)
             $('#deleteImage').attr('onclick', `deleteImg(${result.id})`) // 刪除圖片的按鈕
-          } else { // 沒image_path資料: 增加提示 + 新增圖片 - 刪除圖片
+        } else { // 沒image_path資料: 增加提示 + 新增圖片 - 刪除圖片
             result.image_path = ""
-            $("#previewUpdate~span").text('目前沒有圖片')
+            $("#noImageRecord").text('本文未上傳圖片')
             $("#chooseImage").text('新增圖片')
-            $('#originalImage').text('本文不使用配圖')
-            $('#deleteImage').addClass('d-none')
+            $('#originalImage~span').text('不使用配圖')
+            $('#originalImage').val('')
+            $('#deleteImage').hide()
           }
 
           $('#originalImage').attr('onclick', `originalImg('${result.image_path}')`) // 使用原圖的按鈕
