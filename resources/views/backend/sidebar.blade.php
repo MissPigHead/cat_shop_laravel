2ae975c0590875bf9831ea898e173ca999e23833
Lingling <misspighead@gmail.com>
2021-08-19 17:51:00 +0800

    修改檔名及路徑名稱

--- resources/views/backend/sidebar.blade.php
+++ resources/views/backend/sidebar.blade.php
@@ -1,9 +1,9 @@
 <div class="col-3 sideBar">
   <a href="{{ route('admin') }}"><div>Dashboard</div></a>
   <a href="{{ route('admin.category') }}"><div>商品目錄區</div></a>
-  <a href="{{ route('admin.products') }}"><div>商品區</div></a>
-  <a href="{{ route('admin.members') }}"><div>會員區</div></a>
-  <a href="{{ route('admin.orders') }}"><div>訂單區</div></a>
+  <a href="{{ route('admin.product') }}"><div>商品區</div></a>
+  <a href="{{ route('admin.member') }}"><div>會員區</div></a>
+  <a href="{{ route('admin.order') }}"><div>訂單區</div></a>
   <a href="{{ route('admin.news') }}"><div>育貓新知區</div></a>
   <a href="{{ route('admin.banner') }}"><div>Banner 廣告區</div></a>
 </div>
