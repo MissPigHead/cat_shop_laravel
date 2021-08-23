b4873fbc0b91d36807b3b169844bc6abed6f2b81
Lingling <misspighead@gmail.com>
2021-08-23 14:02:50 +0800

    寫user & order 關聯時order 的種子漏了user_id...

--- database/seeds/OrderSeeder.php
+++ database/seeds/OrderSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Models\Order;
 use App\Models\OrderDetail;
+use App\Models\Recipient;
 
 class OrderSeeder extends Seeder
 {
@@ -16,8 +17,10 @@ class OrderSeeder extends Seeder
         Order::truncate();
         for ($i = 0; $i < 10; $i++) {
             $o = Order::make();
+            $r_id=rand(1,20);
             $o->amount_raw = $o->amount_total = OrderDetail::where('order_id', $i + 1)->sum('amount');
-            $o->recipient_id = rand(1, 20);
+            $o->recipient_id = $r_id;
+            $o->user_id = Recipient::find($r_id)->user_id;
             $o->save();
         }
     }
